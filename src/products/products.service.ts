8855ae844c8ffe3ef186739e916da9bdd43c2a8c
AbdielPerez <abdiel2012p@gmail.com>
2025-05-22 13:16:11 -0600

    feat/pagination implemented

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -69,8 +70,13 @@ export class ProductsService {
 
   }
 
-  async findAll() {
-    return await this.productRepository.find();
+  async findAll(paginationDto: PaginationDto) {
+    const {limit=10, offset=0} = paginationDto
+    return await this.productRepository.find({
+      take:limit,
+      skip:offset
+      //TODO relations
+    });
   }
 
   async findOne(id: string) {
