4358419b87d6e15c52cc93ef6acbe26ffc104921
AbdielPerez <abdiel2012p@gmail.com>
2025-05-23 11:26:52 -0600

    feat/relation product-image ONeToMany created

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "./product.entity";
 
 @Entity()
 export class ProductImage {
@@ -10,6 +11,18 @@ export class ProductImage {
     //Así como lo estoy definiendo siempre será requerido el url en la db
     @Column('text')
     url: string;
+
+    @ManyToOne(
+        ()=>Product,
+        (product) => product.images
+    )
+    product: Product
+    /*
+    Esto no es una columna en ls db (@Column()), sino que es
+    una relación, un montón de imagenes pueden tener un
+    único producto.
+    Muchos registros de mi tabla image pueden tener un único, uno o más producto
+    */
 }
 
 /*
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,6 +1,7 @@
 //Esto es lo que va a buacar mi TypeORM para crear mi db
 
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
 @Entity() //Para que si sea un entity para la db
 export class Product {
@@ -55,6 +56,24 @@ export class Product {
         default: []
     })
     tags: string[];
+
+    //Un producto puede tener muchas img
+    @OneToMany(
+        ()=>ProductImage, //va a regresar un ProductImage
+        productImage => productImage.product,
+        {
+            cascade: true
+            /*
+            Esto hace que cuando eliminpo un producto, 
+            esto elimina las imagenes que están asociadas al producto.
+            Aunque recuerda que esto de eliminar en fuerte de la db solo es para
+            este caso didṕactico, en la vida real no es bueno eliminar
+            en fuerte cosas de la db con el fin de mantener la integridad de los datos
+            y referencias en la db
+            */
+        }
+    )
+    images?: ProductImage
 
     //Se ejecuta siempre antes de hacer una inserción a la db
     @BeforeInsert()
